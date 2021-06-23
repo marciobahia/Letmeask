0197f3a4c0b6d8f3bd45331dd93c76ed861c78e9
marciobahia <marciosellabahia@hotmail.com>
2021-06-23 16:56:26 -0300

    Fluxo de Sala

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import { Home } from "./pages/Home";
+import { Home } from './pages/Home'
+import { NewRoom } from './pages/NewRoom'
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom' 
+
+import { AuthContextProvider } from './contexts/AuthContext'
+import { Room } from './pages/Room';
 
 function App() {
   return (
-   <Home />
-  )
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Switch>
+          <Route component={Home} path="/" exact />
+          <Route component={NewRoom} exact path="/rooms/new" />
+          <Route component={Room} path="/rooms/:id" />
+        </Switch>
+      </AuthContextProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
- 
